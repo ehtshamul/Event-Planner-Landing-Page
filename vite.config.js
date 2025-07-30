acb96a5bc22b5cd539691e0a9a85a4e1d7b96b17
ehtsham ul haq <128890292+ehtshamul@users.noreply.github.com>
2025-07-30 16:24:59 +0500

    Update vite.config.js
    Signed-off-by: ehtsham ul haq  <128890292+ehtshamul@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Event-Planner-Landing-Page/', // <-- your repo name here
   plugins: [react()],
 })
